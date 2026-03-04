4b9f6abe662a6e4c983e44dacb4ea95a1b066a79
Carlos Ramos <cer049@uark.edu>
2026-03-04 10:39:05 -0600

    Fixed typos and added math operations

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-
+# Prompt the user for their name
 name = input('Please input your name: ')
 
 print("hello world")
 
-print("hello, ", name, " I hope you have a great day.")
+# Personalized greeting based on user's name input
+print("hello, ", name, "I hope you have a great day.")
+
+# Some math
+print(7+7*9)
+print((7+7)*9)
