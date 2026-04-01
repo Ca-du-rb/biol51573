3ca88f4b9cb5c4c6346d83cebe6d2498cdf3285c
Carlos Ramos <cer049@uark.edu>
2026-04-01 10:39:02 -0500

    all functions work now

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -17,31 +17,40 @@ def get_args():
     # on the command line, so the default for 'store_true' is actually false (no optional argument specified)
     parser.add_argument("-v", "--verbose", help="Print verbose output", action='store_true')
 
-    # parse (pick apart and interpret) the arguments
+    # parse (pick apart and interpret) the arguments and return in two steps
     args = parser.parse_args()
+    return args
 
+    # or, parse the arguments and return in one step
+    # return(parser.parse_args())
 
 ###-------- function to calculate the Fibonacci number
 def fib():
     # initialize two integers
     a,b = 0,1
 
-    for i in range(int(args.position)):
+    for i in range(int(beyonce.position)):
         a,b = b,a+b
 
     fibonacci_number = a
-
+    return fibonacci_number
 
 ###-------- function to print the output
-def print_output():
-    if args.verbose:
-        print(f"The Fibonacci number in position {args.position} is {fibonacci_number}.")
+def print_output(fibn):
+    if beyonce.verbose:
+        print(f"The Fibonacci number in position {beyonce.position} is {fibn}.")
     else:
-        print(fibonacci_number)
+        print(fibn)
 
-##-------- define a main() function
+###-------- define a main() function
 def main():
+    fibnum = fib()
+    print_output(fibnum)
 
+
+
+###-------- calling get_args happens out here on its own
+beyonce = get_args() # arguments passed by args are always global, but here we assigned that output to beyonce
 
 # set the environment for this script
 # is this main (i.e., a standalone Python script), or
